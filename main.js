79459eb80867e95ec05eac0091358e8c73996c4a
rolling-spirit <83503744+rolling-spirit@users.noreply.github.com>
2022-02-16 22:18:40 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+var canvas = new fabric.Canvas('BdayCanvas');
+ var x= document.getElementById("myAudio");
+
+function new_image()
+{
+
+fabric.Image.fromURL( 'BirthdayImage.jpg',function(Img){
+    block_image_object = Img;
+
+    block_image_object = scaleToWidth(700);
+    block_image_object = scaleToHeight(510);
+    block_image_object.set({
+       top:0,
+       left:0 
+    });
+    canvas.add(block_image_object);
+    });
+}
+{
+	
+	
+}
+
+function playSound(){
+    x.play();
+}	
+
+
+
+
+      fabric.Image.fromURL(get_Image,function (Img) {
+              block_image_object.set({
+                  top: 0,
+                  left: 0
+              });
+              canvas.add(block_image_object);
+          });
